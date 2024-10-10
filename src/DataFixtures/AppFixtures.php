4e7f79a543f88527ec886551cbd7089a78a98fa4
Lucas Martignon <lucas.martignon@wanadoo.fr>
2024-10-10 14:33:48 +0200

    add Fixtures for users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -14,19 +14,44 @@ class AppFixtures extends Fixture
 
   public function load(ObjectManager $manager): void
   {
-    $admin = new Users();
-  
-    $admin->setEmail('lucas.martignon@injject.com');
-    $admin->setLastname('Admin');
-    $admin->setFirstname('Admin');
-    $admin->setUsername("Admin_Admin");
-    $admin->setPassword(
-        $this->passwordHasher->hashPassword($admin, 'admin')
-    );
-    $admin->setRoles(["ROLE_USER"]);
 
-    $manager->persist($admin);
+    $users_array = json_decode(file_get_contents('users.json'), true);
+    foreach($users_array as $index => $user) {
+      $admin = new Users();
+    
+      $admin->setEmail($user['email']);
+      $admin->setLastname($user['lastname']);
+      $admin->setFirstname($user['firstname']);
+
+      $username = strtolower($user['firstname']) ."." . strtolower($user['lastname']);
+      $admin->setUsername($username);
+
+      $password = $this->generateRandomString();
+      $admin->setPassword(
+          $this->passwordHasher->hashPassword($admin, $password)
+      );
+      $admin->setRoles(["ROLE_USER"]);
+
+      $users_array[$index]['password'] = $password;
+
+      $manager->persist($admin);
+    }
+
+    file_put_contents('users.json', json_encode($users_array, JSON_PRETTY_PRINT));
 
     $manager->flush();
   }
+
+  private function generateRandomString($length = 20) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $special_characters = '&?!$%';
+    $randomString = str_shuffle(
+      substr(
+        substr(
+          str_shuffle($special_characters), 0, 2
+        ) . str_shuffle($characters), 0, $length
+      )
+    );
+    return $randomString;
+}
 }
